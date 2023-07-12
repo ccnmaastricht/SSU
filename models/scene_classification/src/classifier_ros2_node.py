552efb33290ece2c7d416b7d4d16180cfc6834cb
MSenden <mario.senden@gmail.com>
2023-07-12 14:41:13 +0200

    updated classification module

--- models/scene_classification/src/classifier_ros2_node.py
+++ models/scene_classification/src/classifier_ros2_node.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd
 from scene_classifier_code import SceneClassificationModel
+from retinal_sampling.sampling_code import GanglionSampling
 
 class ClassifierROS2Node(Node):
     def __init__(self):
@@ -23,13 +24,19 @@ class ClassifierROS2Node(Node):
         self.node_time = 0.0
         self.central_time = 0.0
         self.shut_down = False
+        self.waiting = False
         self.message = None
+
+        # sampling model
+        with open("./retinal_sampling/sampling_parameters.json") as file:
+            sampling_parameters = json.load(file)
+        self.sampler = GanglionSampling(sampling_parameters)
 
         # classification model
         self.device = torch.device('cpu')
         with open("./classification_model/model_parameters.json") as file:
-            parameters = json.load(file)
-        self.classmodel = SceneClassificationModel(parameters)
+            model_parameters = json.load(file)
+        self.classmodel = SceneClassificationModel(model_parameters)
         self.classmodel.load_state_dict(torch.load("./classification_model/saved_model.pt", map_location=self.device))
         self.classmodel.eval()
         self.recurrent = self.classmodel.init_recurrent_state()
@@ -42,16 +49,21 @@ class ClassifierROS2Node(Node):
         self.finished_pub = self.create_publisher(Bool, '/finished', 10)
 
         # subscribers
-        self.snapshot_sub = self.create_subscription(Float32, '/sampling_node/snapshot', self.snapshot_callback, 10)
-        self.eye_pos_sub = self.create_subscription(Float32, '/camera_node/eye_pos', self.eye_pos_callback, 10)
+        self.snapshot_sub = self.create_subscription(Float32, '/camera_node/snapshot', self.snapshot_callback, 10)
+        self.waiting_sub = self.create_subscription(Bool, '/camera_node/waiting', self.waiting_callback, 10)
+        self.eye_pos_sub = self.create_subscription(Float32, '/saccade_node/eye_pos', self.eye_pos_callback, 10)
         self.shut_down_sub = self.create_subscription(Bool, '/sync_node/shutdown', self.shutdown_callback, 10)
 
         # start the loop
         self.classification_loop()
 
-    # Calback functions
+    # Callback functions
     def snapshot_callback(self, msg):
-        self.classmodel.set_snapshot(msg.data)
+        resampled = np.clip(self.sampler.resample_image(msg.data) / 127.5 - 1, -1, 1)
+        self.classmodel.set_snapshot(resampled)
+
+    def waiting_callback(self, msg):
+        self.waiting = msg.data
 
     def eye_pos_callback(self, msg):
         self.classmodel.set_eye_pos(msg.data)
@@ -64,7 +76,8 @@ class ClassifierROS2Node(Node):
         self.central_time = self.get_clock().now().to_msg().sec + self.get_clock().now().to_msg().nanosec * 1e-9
 
     def set_snapshot(self,snapshot):
-        self.snapshot = torch.from_numpy(np.array(snapshot)).permute(0, 3, 1, 2).float().to(self.device)
+        if not np.isscalar(snapshot):
+            self.snapshot = torch.from_numpy(np.array(snapshot)).permute(0, 3, 1, 2).float().to(self.device)
     
     def set_eye_pos(self,eye_pos):
         self.eye_pos = torch.from_numpy(np.array(eye_pos)).float().to(self.device)
@@ -92,6 +105,10 @@ class ClassifierROS2Node(Node):
             
             if self.node_time>=self.central_time:
                 # wait for next time step
+                continue
+
+            if self.waiting:
+                # wait for snapshot
                 continue
 
             # Run the classification model on the current snapshot and eye position
